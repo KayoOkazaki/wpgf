29022e8aa26e8028621abd66ccb2bb5a0add7556
zd2B03 <rosafleur.brise@gmail.com>
2017-07-11 10:05:46 +0900

    データ移行用のフォルダを作成（wp-content）、(データベース更新バッチ)

---
+++ データ移行/DB更新バッチSQL/zd2b03.sim.zdrv.com/wp-config(DBの設定).php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * WordPress の基本設定
+ *
+ * このファイルは、インストール時に wp-config.php 作成ウィザードが利用します。
+ * ウィザードを介さずにこのファイルを "wp-config.php" という名前でコピーして
+ * 直接編集して値を入力してもかまいません。
+ *
+ * このファイルは、以下の設定を含みます。
+ *
+ * * MySQL 設定
+ * * 秘密鍵
+ * * データベーステーブル接頭辞
+ * * ABSPATH
+ *
+ * @link http://wpdocs.osdn.jp/wp-config.php_%E3%81%AE%E7%B7%A8%E9%9B%86
+ *
+ * @package WordPress
+ */
+
+// 注意:
+// Windows の "メモ帳" でこのファイルを編集しないでください !
+// 問題なく使えるテキストエディタ
+// (http://wpdocs.osdn.jp/%E7%94%A8%E8%AA%9E%E9%9B%86#.E3.83.86.E3.82.AD.E3.82.B9.E3.83.88.E3.82.A8.E3.83.87.E3.82.A3.E3.82.BF 参照)
+// を使用し、必ず UTF-8 の BOM なし (UTF-8N) で保存してください。
+
+// ** MySQL 設定 - この情報はホスティング先から入手してください。 ** //
+/** WordPress のためのデータベース名 */
+define('DB_NAME', 'DBzd2B03');
+
+/** MySQL データベースのユーザー名 */
+define('DB_USER', 'zd2B03');
+
+/** MySQL データベースのパスワード */
+define('DB_PASSWORD', '3SXK6X');
+
+/** MySQL のホスト名 */
+define('DB_HOST', 'localhost');
+
+/** データベースのテーブルを作成する際のデータベースの文字セット */
+define('DB_CHARSET', 'utf8mb4');
+
+/** データベースの照合順序 (ほとんどの場合変更する必要はありません) */
+define('DB_COLLATE', '');
+define('FS_METHOD', 'direct');
+
---
+++ データ移行/wp-content/readme.txt
@@ -0,0 +1,6 @@
+データ移行時、WordPressプロジェクトフォルダ配下にある
+wp-contentフォルダへ以下のフォルダごと上書きコピーする。
+
+plugins   WordPressプラグインのデータ
+upgrade   アップロードしたメディア
+
